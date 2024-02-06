599bb92e763cc5522ecf589a66951e89d709f487
dewo952 <kabirkhatri1998@gmail.com>
2024-02-06 20:31:54 +0530

    updates

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
-
 const Banner = () => {
   return (
     <main>
-      <div className="px-6 lg:px-8 relative">
-        {/* Container for Image and GIF */}
-        <div className="mx-auto max-w-7xl pt-16 sm:pt-20 pb-20 banner-image relative">
-          {/* Your existing text content */}
+      <div className="px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl pt-16 sm:pt-20 pb-20 banner-image">
           <div className="text-center">
             <h1 className="text-5xl font-semibold text-navyblue sm:text-5xl lg:text-5xl md:4px">
               Elevate Your Restaurant&apos;s Performance
@@ -22,7 +19,6 @@ const Banner = () => {
             </p>
           </div>
 
-          {/* Buttons */}
           <div className="text-center mt-5">
             <Link href="/components/exploreInsights">
               <button className="text-15px text-white font-medium bg-blue py-5 px-9 mt-2 leafbutton">
@@ -36,25 +32,12 @@ const Banner = () => {
             </Link>
           </div>
 
-          <div className="relative z-0">
-            <Image
-              src="/assets/banner/dashboard.svg"
-              alt="banner-image"
-              width={1200}
-              height={598}
-            />
-
-            {/* GIF now placed within the same container as the SVG */}
-            <div className="absolute top-0 left-0 right-0 bottom-0">
-              <Image
-                src="/assets/banner/homescreen.gif"
-                alt="GIF on top"
-                layout="responsive"
-                width={800}
-                height={400}
-              />
-            </div>
-          </div>
+          <Image
+            src={"/assets/banner/dashboard.svg"}
+            alt="banner-image"
+            width={1200}
+            height={598}
+          />
         </div>
       </div>
     </main>
